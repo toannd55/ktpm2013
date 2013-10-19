e050e88e24d4757a40f5283dee336307ef854d27
Đức Toàn Nguyễn <nguyenductoan092@gmail.com>
2013-10-19 21:10:40 +0700

    new

--- test_generation/output.py
+++ test_generation/output.py
@@ -1,4 +1,6 @@
-#Output
+#Ho va ten: Nguyen Duc Toan
+#MSSV: 10020361
+#output.py
 
 import input
 import math
@@ -14,85 +16,82 @@ arr = [] #luu cac bien de kiem tra lop tuong duong
 count = -1
 leng = len(doc)
 test = 0 #kiem tra xem du 2 dau cua lop tuong duong
-check = True
 a = 0
 b = 0
 ix=0
 iy=0
 
 #luu du lieu input vao 2 mang
-while i<leng:
-    temp = ''
-    if doc[i] == '[':
-        for j in range(i+1, leng):
-            if doc[j] != ';':
-                temp += doc[j]
-            else:
-                break
-        a = int(temp)
-        arr[count].append(a)
+try:
+    while i<leng:
         temp = ''
-    if doc[i] == ';':
-        for k in range(i+1, leng):
-            if doc[k] != ']':
-                temp += doc[k]
-            else:
-                break
-        b = int(temp)
-        arr[count].append(b)
-        temp = ''
-        test = 1
-    if test == 1:
-        mid = (a+b)/2 #gia tri trung binh cua moi khoang
-        #array[count].append(a)
-        #array[count].append(a-1)
-        #array[count].append(b)
-        #array[count].append(b+1)
-        array[count].append(mid)
-        test = 0
-    if doc[i] == ':':
-        array.append([])
-        arr.append([])
-        count += 1
-    i += 1
+        if doc[i] == '[':
+            for j in range(i+1, leng):
+                if doc[j] != ';':
+                    temp += doc[j]
+                else:
+                    break
+            a = int(temp)
+            arr[count].append(a)
+            temp = ''
+        if doc[i] == ';':
+            for k in range(i+1, leng):
+                if doc[k] != ']':
+                    temp += doc[k]
+                else:
+                    break
+            b = int(temp)
+            arr[count].append(b)
+            temp = ''
+            test = 1
+        if test == 1:
+            mid = (a+b)/2 #gia tri trung binh cua moi khoang
+            array[count].append(mid)
+            test = 0
+        if doc[i] == ':':
+            array.append([])
+            arr.append([])
+            count += 1
+        i += 1
+    print array
+    #Sap xep theo dau mut trai cua tung khoang de kiem tra t/m lop tuong duong k?    
+    for arr2 in arr:
+        for ix in xrange(0, len(arr2)):
+            if ix%2==0:
+                for iy in xrange(ix+2, len(arr2)):
+                    if iy%2==0:
+                        if arr2[ix] > arr2[iy]:
+                            numx = arr2[ix]
+                            numy = arr2[ix+1]
+                            arr2[ix] = arr2[iy]
+                            arr2[ix+1] = arr2[iy+1]
+                            arr2[iy] = numx
+                            arr2[iy+1] = numy
 
-#Sap xep theo dau mut trai cua tung khoang de kiem tra t/m lop tuong duong k?    
-for arr2 in arr:
-    for ix in xrange(0, len(arr2)):
-        if ix%2==0:
-            for iy in xrange(ix+2, len(arr2)):
-                if iy%2==0:
-                    if arr2[ix] > arr2[iy]:
-                        numx = arr2[ix]
-                        numy = arr2[ix+1]
-                        arr2[ix] = arr2[iy]
-                        arr2[ix+1] = arr2[iy+1]
-                        arr2[iy] = numx
-                        arr2[iy+1] = numy
+    #kiem tra lop tuong duong
+    for arr2 in arr:
+        for ix in xrange(0, len(arr2)-1):
+            if ix%2==1:
+                if arr2[ix] >= arr2[ix+1]:
+                    raise Exception("wrong input")
+                    break
+            if ix%2==0:
+                if arr2[ix] >= arr2[ix+1]:
+                    raise Exception("wrong input")
+                    break
 
-#kiem tra lop tuong duong
-for arr2 in arr:
-    for ix in xrange(0, len(arr2)-1):
-        if ix%2==1:
-            if arr2[ix] >= arr2[ix+1]:
-                check = False
-                break
-        if ix%2==0:
-            if arr2[ix] >= arr2[ix+1]:
-                check = False
-                break
-
-class autoTest(unittest.TestCase):
-    def __init__(self, *args):
-        super(autoTest, self).__init__()
-        self.args = args
-    def runTest(self):
-        self.assertEqual(input.main(*self.args), 1)
-    
-if __name__ == '__main__':
-    if(check == True):
-        suite = unittest.TestSuite()
-        suite.addTests(autoTest(*i) for i in itertools.product(*array))
-        unittest.TextTestRunner().run(suite)
-    else:
-        print "wrong input"
+    class autoTest(unittest.TestCase):
+        pass
+    def testCase(*args):
+        def test(self):
+            self.assertEqual(input.main(*args),0)
+        return test
+		
+    if __name__ == '__main__':
+        for element in itertools.product(*array):
+            name = 'test_' + str(element)
+            test = testCase(*element)
+            setattr(autoTest, name, test)
+        unittest.main()
+except Exception as ex:
+    print "wrong input"
